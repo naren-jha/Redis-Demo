49480dd10e7c5a455598b426ffeabc0698294536
Narendra Jha <njha.sde@gmail.com>
2022-04-14 18:03:47 +0530

    doc comment

--- src/main/java/com/njha/config/ClusterConfigurationProperties.java
+++ src/main/java/com/njha/config/ClusterConfigurationProperties.java
@@ -9,12 +9,15 @@ import java.util.List;
 @ConfigurationProperties(prefix = "spring.redis.cluster")
 public class ClusterConfigurationProperties {
 
+    // https://stackoverflow.com/questions/41918560/redis-cluster-integration-with-spring-boot
     /*
      * spring.redis.cluster.nodes[0] = 127.0.0.1:7000
      * spring.redis.cluster.nodes[1] = 127.0.0.1:7001
      * spring.redis.cluster.nodes[1] = 127.0.0.1:7002
      */
     List<String> nodes;
+
+
 
     /**
      * Get initial collection of known cluster nodes in format {@code host:port}.
