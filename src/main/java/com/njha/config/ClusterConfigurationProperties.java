924fa2a87b75fd50384efe782cf3ae0242383002
Narendra Jha <njha.sde@gmail.com>
2022-04-14 07:25:23 +0530

    cluster mode jedis

---
+++ src/main/java/com/njha/config/ClusterConfigurationProperties.java
@@ -0,0 +1,31 @@
+package com.njha.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@ConfigurationProperties(prefix = "spring.redis.cluster")
+public class ClusterConfigurationProperties {
+
+    /*
+     * spring.redis.cluster.nodes[0] = 127.0.0.1:7000
+     * spring.redis.cluster.nodes[1] = 127.0.0.1:7001
+     * ...
+     */
+    List<String> nodes;
+
+    /**
+     * Get initial collection of known cluster nodes in format {@code host:port}.
+     *
+     * @return
+     */
+    public List<String> getNodes() {
+        return nodes;
+    }
+
+    public void setNodes(List<String> nodes) {
+        this.nodes = nodes;
+    }
+}
--- src/main/java/com/njha/config/RedisConfig.java
+++ src/main/java/com/njha/config/RedisConfig.java
@@ -1,7 +1,9 @@
 package com.njha.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisClusterConfiguration;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -17,6 +19,8 @@ public class RedisConfig {
     So we'll need these two beans here.
      */
 
+    // Standalone mode jedisConnectionFactory
+    /*
     @Bean
     public JedisConnectionFactory jedisConnectionFactory() {
         RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
@@ -28,7 +32,26 @@ public class RedisConfig {
         // here we can set attributes for jedisConnectionFactory - like maxPoolSize, timeouts, etc
         return jedisConnectionFactory;
     }
+    */
 
+
+    // Cluster mode jedisConnectionFactory
+    @Autowired
+    ClusterConfigurationProperties clusterProperties;
+
+    @Bean
+    public JedisConnectionFactory jedisConnectionFactory() {
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+        redisStandaloneConfiguration.setHostName("127.0.0.1");
+        redisStandaloneConfiguration.setPort(6379);
+        //redisStandaloneConfiguration.setPassword("password");
+
+        JedisConnectionFactory jedisConnectionFactory = new JedisConnectionFactory(new RedisClusterConfiguration(clusterProperties.getNodes()));
+        // here we can set attributes for jedisConnectionFactory - like maxPoolSize, timeouts, etc
+        return jedisConnectionFactory;
+    }
+
+    // RedisTemplate remains unchanged irrespective of the mode
     @Bean
     public RedisTemplate<String, Object> redisTemplate() {
         RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
